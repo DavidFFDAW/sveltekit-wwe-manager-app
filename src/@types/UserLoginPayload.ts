db52cdf863d6d4c0aeedb3f49a6f12b7d148c5df
DavidFFDAW <davidferflo2@gmail.com>
2025-01-21 18:03:27 +0100

    feat: middleware for login

---
+++ src/@types/UserLoginPayload.ts
@@ -0,0 +1,9 @@
+import type { JwtPayload } from 'jsonwebtoken';
+
+export interface UserLoginPayload extends JwtPayload {
+	uuid: number;
+	name: string;
+	email: string;
+	role: string;
+	username: string;
+}
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,5 +1,16 @@
 import { JWT } from '$lib/server/jwt.helper';
 import { redirect, type Handle } from '@sveltejs/kit';
+import type { UserLoginPayload } from './@types/UserLoginPayload';
+
+const getUserFromPayloadToLocals = (payload: UserLoginPayload) => {
+	return {
+		uuid: payload.uuid,
+		name: payload.name,
+		role: payload.role,
+		username: payload.username,
+		email: payload.email
+	};
+};
 
 export const handle: Handle = async ({ event, resolve }) => {
 	const isAdminRequestedRoute = event.url.pathname.startsWith('/admin');
@@ -10,21 +21,22 @@ export const handle: Handle = async ({ event, resolve }) => {
 
 	if (isLoginRequestedRoute) {
 		if (!sessionToken) return resolve(event);
-		const payload = JWT.verify(sessionToken);
-		if (!payload) return resolve(event);
+		const payload = JWT.verify(sessionToken) as UserLoginPayload;
+		if (!payload) {
+			event.locals.user = undefined;
+			event.cookies.delete('session', { path: '/' });
+			return resolve(event);
+		}
+		event.locals.user = getUserFromPayloadToLocals(payload);
+		throw redirect(302, '/admin');
 	}
 
 	if (isAdminRequestedRoute) {
 		if (!sessionToken) throw redirect(302, '/login');
+		const payload = JWT.verify(sessionToken);
+		if (!payload) throw redirect(302, '/login');
+		event.locals.user = getUserFromPayloadToLocals(payload as UserLoginPayload);
 
-		event.locals.user = {
-			uuid: 45,
-			name: 'admin',
-			role: 'admin',
-			username: 'admin',
-			email: 'test'
-		};
-		console.log('Check for user authentication');
 		return resolve(event);
 	}
 
