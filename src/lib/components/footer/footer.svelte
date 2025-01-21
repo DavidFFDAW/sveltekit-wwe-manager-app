e1fe803311a4863d6d0e4b9d197b130b6c22f41b
DavidFFDAW <davidferflo2@gmail.com>
2025-01-22 00:14:32 +0100

    fix: redirections on footer links

--- src/lib/components/footer/footer.svelte
+++ src/lib/components/footer/footer.svelte
@@ -23,18 +23,22 @@
 		<div class="footer-content-item footer-item">
 			<p class="footer-title">Páginas de interés</p>
 			<ul class="footer-pages-list list-style-none">
-				<li class="footer-list-item"><a class="footer-link" href="/contact">Contacto</a></li>
-				<li class="footer-list-item"><a class="footer-link" href="/about">Sobre nosotros</a></li>
 				<li class="footer-list-item">
-					<a class="footer-link" href="/pages/champions">Campeones</a>
+					<a class="footer-link" href="/contact">Contacto</a>
 				</li>
+				<li class="footer-list-item">
+					<a class="footer-link" href="/about">Sobre nosotros</a>
+				</li>
+				<!-- <li class="footer-list-item">
+					<a class="footer-link" href="/pages/champions">Campeones</a>
+				</li> -->
 				<li class="footer-list-item"><a class="footer-link" href="/blog">Blog</a></li>
 			</ul>
 		</div>
 		<div class="footer-content-item footer-item">
 			<p class="footer-title">Páginas relevantes</p>
 			<ul class="footer-pages-list list-style-none">
-				<li class="footer-list-item">
+				<!-- <li class="footer-list-item">
 					<a class="footer-link" href="/pages/roster">Roster</a>
 				</li>
 				<li class="footer-list-item">
@@ -43,7 +47,7 @@
 				<li class="footer-list-item">
 					<a class="footer-link" href="/pages/historic">Histórico de campeones</a>
 				</li>
-				<li class="footer-list-item"><a class="footer-link" href="/twitter">Twitter</a></li>
+				<li class="footer-list-item"><a class="footer-link" href="/twitter">Twitter</a></li> -->
 			</ul>
 		</div>
 		<div class="footer-content-item footer-item">
---
+++ src/routes/(public)/(statics)/about/+page.svelte
@@ -0,0 +1,1 @@
+<h1>Sobre la app</h1>
