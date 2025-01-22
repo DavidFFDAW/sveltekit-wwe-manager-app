bfaead4bd6bd7e72e61ee99f2118052634c1fc0e
DavidFFDAW <davidferflo2@gmail.com>
2025-01-22 14:18:28 +0100

    refactor: login refactor to look better

--- src/routes/(public)/login/+page.svelte
+++ src/routes/(public)/login/+page.svelte
@@ -6,52 +6,54 @@
 </script>
 
 <PageWrapper page="login">
-	<div class="login-container">
-		<div class="flex column acenter center">
-			<h1><img width="96" height="96" src="/icons/versatile.png" alt="" /></h1>
-			<h2><span class="dreadnotus">Acceder</span></h2>
-			<div class="down login-auth-form-container">
-				<AsyncForm method="post" showButtons={false} redirect="/admin">
-					<div class="flex center gap-small column astart relative" style="height:205px">
-						<input
-							type="text"
-							placeholder="example@email.com"
-							autocomplete="email"
-							aria-autocomplete="list"
-							class="w1 app-custom-input"
-							name="email"
-							required
-						/>
-						<div class="w1 flex between">
+	<div class="login-container-wrapper">
+		<div class="login-container">
+			<div class="flex column acenter center">
+				<h1><img width="96" height="96" src="/icons/versatile.png" alt="" /></h1>
+				<h2><span class="dreadnotus">Acceder</span></h2>
+				<div class="down login-auth-form-container">
+					<AsyncForm method="post" showButtons={false} redirect="/admin">
+						<div class="flex center gap-small column astart relative" style="height:205px">
 							<input
-								type={showPassword ? 'text' : 'password'}
-								placeholder="Password"
-								class="w1 app-custom-input nradius"
-								autocomplete="current-password"
+								type="text"
+								placeholder="example@email.com"
+								autocomplete="email"
 								aria-autocomplete="list"
-								name="password"
+								class="w1 app-custom-input"
+								name="email"
 								required
 							/>
-							<button
-								class="nradius pointer btn cta"
-								type="button"
-								on:click={() => (showPassword = !showPassword)}
-							>
-								{showPassword ? 'Ocultar' : 'Mostrar'}
-							</button>
+							<div class="w1 flex between">
+								<input
+									type={showPassword ? 'text' : 'password'}
+									placeholder="Password"
+									class="w1 app-custom-input nradius"
+									autocomplete="current-password"
+									aria-autocomplete="list"
+									name="password"
+									required
+								/>
+								<button
+									class="nradius pointer btn cta"
+									type="button"
+									on:click={() => (showPassword = !showPassword)}
+								>
+									{showPassword ? 'Ocultar' : 'Mostrar'}
+								</button>
+							</div>
+							<a href="#forgot" class="w1 block forgot opensans fw-800 tcenter">
+								He olvidado mi contraseña
+							</a>
 						</div>
-						<a href="#forgot" class="w1 block forgot opensans fw-800 tcenter">
-							He olvidado mi contraseña
-						</a>
-					</div>
 
-					<div class="w1 flex between row">
-						<a href="/register">
-							<button type="button" class="w1 btn"> Registrar </button>
-						</a>
-						<button class="btn cta" type="submit">Acceder</button>
-					</div>
-				</AsyncForm>
+						<div class="w1 flex between row">
+							<a href="/register">
+								<button type="button" class="w1 btn"> Registrar </button>
+							</a>
+							<button class="btn cta" type="submit">Acceder</button>
+						</div>
+					</AsyncForm>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -70,5 +72,19 @@
 		border-radius: 5px;
 		box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 		text-align: center;
+	}
+
+	@media screen and (max-width: 768px) {
+		.login-container-wrapper {
+			height: 100%;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			flex-direction: column;
+		}
+		.login-container {
+			position: static;
+			transform: translate(0);
+		}
 	}
 </style>
