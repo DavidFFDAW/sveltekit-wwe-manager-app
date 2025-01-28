ad72d2c59e2c66a43fa8acd243aa45c8eea32cae
DavidFFDAW <davidferflo2@gmail.com>
2025-01-28 10:04:53 +0100

    feat: new disposition of dialogs with custom app dialog

---
+++ src/lib/components/dialog/dialog.svelte
@@ -0,0 +1,82 @@
+<script lang="ts">
+	import Icon from '../icons/icon.svelte';
+
+	let dialog: HTMLDialogElement;
+	export let opened: boolean;
+
+	const closeDialog = () => {
+		dialog.close();
+		opened = false;
+	};
+
+	$: {
+		if (opened) dialog.showModal();
+		if (!opened && dialog) dialog.close();
+	}
+</script>
+
+<dialog class="app-general-dialog" bind:this={dialog}>
+	<header class="app-general-dialog-header">
+		<button on:click={closeDialog} type="button" class="app-general-dialog-close">
+			<Icon icon="x" />
+		</button>
+	</header>
+
+	<div class="app-general-dialog-content">
+		<slot></slot>
+	</div>
+</dialog>
+
+<style>
+	.app-general-dialog {
+		position: fixed;
+		top: 50%;
+		left: 50%;
+		width: 90%;
+		height: 90%;
+		outline: none;
+		background-color: #fff;
+		z-index: 10000000;
+		transform: translate(-50%, -50%);
+		border-radius: 10px;
+		box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+		border: 0;
+	}
+
+	.app-general-dialog::backdrop {
+		background-color: rgba(0, 0, 0, 0.6);
+	}
+
+	.app-general-dialog header.app-general-dialog-header {
+		display: flex;
+		justify-content: flex-end;
+		width: 100%;
+		background-color: #f1f1f1;
+	}
+
+	.app-general-dialog header.app-general-dialog-header button.app-general-dialog-close {
+		color: #fff;
+		border-radius: 0;
+		padding: 10px;
+		background-color: #c91727;
+	}
+
+	.app-general-dialog .app-general-dialog-content {
+		padding: 1rem;
+	}
+
+	dialog.app-general-dialog[open] {
+		animation: fadeIn 0.3s;
+	}
+
+	@keyframes fadeIn {
+		from {
+			transform: translate(-50%, -50%) scale(0.5);
+			opacity: 0;
+		}
+		to {
+			transform: translate(-50%, -50%) scale(1);
+			opacity: 1;
+		}
+	}
+</style>
--- src/lib/components/gallery/gallery.svelte
+++ src/lib/components/gallery/gallery.svelte
@@ -1,30 +1,25 @@
 <script lang="ts">
-	import Icon from '../icons/icon.svelte';
+	import Dialog from '../dialog/dialog.svelte';
 	import { getGalleryImages, type GalleryImage } from './gallery.service';
 
-	let dialog: HTMLDialogElement;
+	let opened: boolean = false;
 	let images: GalleryImage[] = [];
 	export let value: string = '';
+	let active: string = '';
 
 	const getImages = async () => {
 		const fetchedImages = await getGalleryImages();
 		images = fetchedImages;
-		if (images.length > 0) dialog.showModal();
-	};
-
-	const closeDialog = () => {
-		dialog.close();
+		if (images.length > 0) opened = true;
 	};
 
 	const selectImage = (event: MouseEvent) => {
 		const target = event.target as HTMLImageElement;
-		const htmlImages = document.querySelectorAll('.gallery img');
-		htmlImages.forEach((img) => img.classList.remove('active'));
-		target.classList.add('active');
+		active = target.dataset.url as string;
 
 		if (target.getAttribute('data-url')) {
 			value = target.dataset.url as string;
-			closeDialog();
+			opened = false;
 		}
 	};
 </script>
@@ -32,18 +27,13 @@
 <div>
 	<button type="button" class="btn secondary" on:click={getImages}>Galeria</button>
 
-	<dialog class="gallery-dialog" bind:this={dialog}>
-		<header>
-			<button on:click={closeDialog}>
-				<Icon icon="x" />
-			</button>
-		</header>
-
+	<Dialog bind:opened>
 		<div class="gallery overflow-vertical h1">
 			{#each images as image}
 				<img
 					src={image.url}
 					alt={image.name}
+					class:active={image.url === active}
 					draggable="false"
 					role="presentation"
 					data-url={image.url}
@@ -52,43 +42,18 @@
 				/>
 			{/each}
 		</div>
-	</dialog>
+	</Dialog>
 </div>
 
 <style>
-	.gallery-dialog {
-		position: fixed;
-		top: 50%;
-		left: 50%;
-		width: 90%;
-		height: 90%;
-		outline: none;
-		background-color: #fff;
-		z-index: 10000000;
-		transform: translate(-50%, -50%);
-		border-radius: 10px;
-		box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-		border: 0;
-	}
-
-	.gallery-dialog::backdrop {
-		background-color: rgba(0, 0, 0, 0.6);
-	}
-
-	.gallery-dialog header {
-		display: flex;
-		justify-content: flex-end;
-		padding: 1rem;
-	}
-
-	.gallery-dialog .gallery {
+	.gallery {
 		display: grid;
 		grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
 		gap: 1rem;
 		padding: 1rem;
 	}
 
-	.gallery-dialog img {
+	.gallery img {
 		width: 100%;
 		height: auto;
 		padding: 10px;
@@ -96,7 +61,7 @@
 		cursor: pointer;
 	}
 
-	.gallery-dialog img:hover {
+	.gallery img:hover {
 		border: 1px solid #c91727;
 		background-color: #f9f9f9;
 	}
