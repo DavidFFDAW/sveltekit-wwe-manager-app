a58f5ca29cc4970093d3568d47d7f13dd45ad1ce
DavidFFDAW <davidferflo2@gmail.com>
2025-03-04 00:40:54 +0100

    feat: service worker to add pwa to app

---
+++ src/lib/components/worker/service-worker.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	onMount(async () => {
+		if (!('serviceWorker' in navigator)) {
+			console.warn('Service Worker not supported');
+			return;
+		}
+
+		navigator.serviceWorker
+			.register('/sw.js')
+			.then((reg) => {
+				console.log('Service Worker registered', reg);
+			})
+			.catch((err) => {
+				console.log('Service Worker registration failed', err);
+			});
+	});
+</script>
---
+++ static/sw.js
@@ -0,0 +1,57 @@
+const CACHE_NAME = 'mi-app-cache-v1';
+const URLS_A_CACHEAR = ['/', '/icons/versatile.png'];
+
+// 🚀 Instalación: Cacheamos archivos esenciales
+self.addEventListener('install', (event) => {
+	console.log('[SW] Instalando...');
+	event.waitUntil(
+		caches.open(CACHE_NAME).then((cache) => {
+			console.log('[SW] Cacheando archivos...');
+			return cache.addAll(URLS_A_CACHEAR);
+		})
+	);
+});
+
+// ✅ Activación: Limpieza de caché antigua si cambia la versión
+self.addEventListener('activate', (event) => {
+	console.log('[SW] Activado');
+	event.waitUntil(
+		caches.keys().then((keys) =>
+			Promise.all(
+				keys.map((key) => {
+					if (key !== CACHE_NAME) {
+						console.log('[SW] Eliminando caché antigua:', key);
+						return caches.delete(key);
+					}
+				})
+			)
+		)
+	);
+});
+
+self.addEventListener('push', (event) => {
+	const data = event.data.json();
+
+	self.registration.showNotification(data.title, {
+		body: data.body,
+		icon: '/icons/versatile.png',
+		vibrate: [100, 50, 100],
+		data: { url: data.url }
+	});
+});
+
+self.addEventListener('notificationclick', async (event) => {
+	event.notification.close();
+	const clients = await self.clients.matchAll();
+	if (event.notification.data.url) {
+		event.waitUntil(clients.openWindow(event.notification.data.url));
+	}
+});
+
+// 🔄 Intercepción de peticiones (Fetch)
+// self.addEventListener('fetch', (event) => {
+// 	console.log('[SW] Interceptando petición:', event.request.url);
+// 	event.respondWith(
+// 		caches.match(event.request).then((response) => response || fetch(event.request))
+// 	);
+// });
