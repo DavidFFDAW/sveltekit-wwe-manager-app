7a97d7bbaf106b49b781d0072cdddeb95acb5bc0
DavidFFDAW <davidferflo2@gmail.com>
2025-03-19 13:17:45 +0100

    feat: dashboard new styles and pictures

--- src/routes/admin/dashboard/+page.svelte
+++ src/routes/admin/dashboard/+page.svelte
@@ -18,47 +18,91 @@
 				<ul class="grid three-column-grid gap-20 admin-dashboard-list responsive" role="list">
 					<li class="w1 background">
 						<a href="/admin/wrestlers">
-							Luchadores
+							<span>Luchadores</span>
 							<img
 								src="https://davidfernandezdeveloper.es/2k/images/john-cena.webp"
 								alt="se muestra a John Cena y al pasar por encima se hace más grande saliendo del recuadro"
+								draggable="false"
+								aria-hidden="true"
+								loading="lazy"
 							/>
 						</a>
 					</li>
-					<li class="w1 background reverse">
+					<li class="w1 background">
 						<a href="/admin/blog">
-							Blog
+							<span>Blog</span>
 							<img
 								src="https://davidfernandezdeveloper.es/2k/images/the-rock.webp"
 								alt="se muestra a Roman Reigns y al pasar por encima se hace más grande saliendo del recuadro"
+								draggable="false"
+								aria-hidden="true"
+								loading="lazy"
 							/>
 						</a>
 					</li>
 					<li class="w1 background">
 						<a href="/admin/championship-reigns">
-							Reinados
+							<span>Cinturones</span>
+							<img
+								src="https://davidfernandezdeveloper.es/2k/images/randy-orton.webp"
+								alt="se muestra a Ric Flair y al pasar por encima se hace más grande saliendo del recuadro"
+								draggable="false"
+								aria-hidden="true"
+								loading="lazy"
+							/>
+						</a>
+					</li>
+					<li class="w1 background reverse">
+						<a href="/admin/championship-reigns">
+							<span>Reinados</span>
 							<img
 								src="https://davidfernandezdeveloper.es/2k/images/gunther.webp"
 								alt="se muestra a Gunther y al pasar por encima se hace más grande saliendo del recuadro"
+								draggable="false"
+								aria-hidden="true"
+								loading="lazy"
 							/>
 						</a>
 					</li>
 					<li class="w1 background reverse">
-						<a href="/admin/ppvs">
-							PPVs
+						<a href="/admin/teams">
+							<span>Equipos</span>
+							<img
+								src="https://davidfernandezdeveloper.es/2k/images/bret-hart.webp"
+								alt="se muestra a Bret Hart y al pasar por encima se hace más grande saliendo del recuadro"
+								draggable="false"
+								aria-hidden="true"
+								loading="lazy"
+							/>
+						</a>
+					</li>
+					<li class="w1 background reverse">
+						<a href="/admin/users">
+							<span>Usuarios</span>
 							<img
 								src="https://davidfernandezdeveloper.es/2k/images/shawn-michaels.webp"
-								alt="se muestra a The Undertaker y al pasar por encima se hace más grande saliendo del recuadro"
+								alt="se muestra a Shawn Michaels y al pasar por encima se hace más grande saliendo del recuadro"
+								draggable="false"
+								aria-hidden="true"
+								loading="lazy"
 							/>
 						</a>
 					</li>
-					<li class="w1 background reverse">
+					<li class="w1 background">
 						<a href="/admin/image-editor">
-							Editor
+							<span>Editor de imagenes</span>
 							<img
 								src="https://davidfernandezdeveloper.es/2k/images/kurt-angle.webp"
-								alt="se muestra a Stone Cold y al pasar por encima se hace más grande saliendo del recuadro"
+								alt="se muestra a Kurt Angle y al pasar por encima se hace más grande saliendo del recuadro"
+								draggable="false"
+								aria-hidden="true"
+								loading="lazy"
 							/>
+						</a>
+					</li>
+					<li class="w1 background ppv-item">
+						<a href="/admin/ppv">
+							<span>Eventos y shows</span>
 						</a>
 					</li>
 				</ul>
@@ -70,7 +114,8 @@
 <style>
 	:root {
 		/* --dashboard-item-bg: url('https://cdn.pixabay.com/photo/2016/11/18/22/58/stars-1837306_640.jpg'); */
-		--dashboard-item-bg: url('https://cdn.pixabay.com/photo/2016/11/18/22/58/stars-1837306_640.jpg');
+		/* --dashboard-item-bg: url('https://cdn.pixabay.com/photo/2016/11/18/22/58/stars-1837306_640.jpg'); */
+		--dashboard-item-bg: url('https://media.istockphoto.com/id/1665661357/vector/bright-stadium-lights-vector-design.jpg?s=612x612&w=0&k=20&c=fPARi4dhnmIQprMY4_EneLckTQBVfns1Z1dnwl7CbE8=');
 	}
 	ul.admin-dashboard-list li {
 		height: 200px;
@@ -79,6 +124,12 @@
 		box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 		background-color: #ffffff;
 		background-image: var(--dashboard-item-bg);
+		background-size: cover;
+		background-position: center;
+		background-repeat: no-repeat;
+	}
+	ul.admin-dashboard-list li.ppv-item {
+		background-image: url('https://www.wwe.com/f/styles/gallery_img_l/public/all/2024/01/131_RR_01272024GD_37295--a0647e43afa91c64976613f63e4c387c.jpg');
 	}
 
 	ul.admin-dashboard-list li a {
@@ -87,12 +138,20 @@
 		position: relative;
 		width: 100%;
 		height: 100%;
-		align-items: center;
+		align-items: flex-end;
 		color: #ffffff;
 		text-decoration: none;
 		font-family: 'dreadnotus', sans-serif;
 		text-transform: uppercase;
-		font-size: 28px;
+		font-size: 20px;
+	}
+	ul.admin-dashboard-list li a span {
+		position: relative;
+		z-index: 20;
+		text-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+		display: block;
+		background-color: rgba(0, 0, 0, 0.5);
+		padding: 0.5rem 1rem;
 	}
 
 	ul.admin-dashboard-list li:nth-child(odd) a {
@@ -113,6 +172,7 @@
 		transition: transform 0.3s;
 		transform-origin: bottom;
 		border-radius: 0 0 8px 8px;
+		z-index: 1;
 	}
 
 	ul.admin-dashboard-list li:nth-child(odd) a img {
@@ -144,8 +204,20 @@
 	}
 
 	@media screen and (max-width: 768px) {
+		ul.admin-dashboard-list li img {
+			width: 220px;
+		}
+
 		ul.admin-dashboard-list li a {
 			font-size: 18px;
+			align-items: flex-end;
+		}
+		ul.admin-dashboard-list li a::after {
+			top: calc(100% - 20px);
+		}
+
+		.admin-dashboard-list {
+			gap: 35px;
 		}
 	}
 </style>
