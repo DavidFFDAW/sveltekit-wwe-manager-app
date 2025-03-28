87387265b0274a049eb266f6eaf8f7432519fae7
DavidFFDAW <davidferflo2@gmail.com>
2025-03-28 15:25:10 +0100

    feat: new multistep form design for reign upsertion

---
+++ src/routes/admin/championship-reigns/components/multistep.svelte
@@ -0,0 +1,87 @@
+<script lang="ts">
+	import Box from '$lib/components/box/box.svelte';
+	import type { ReignUpsertProps } from '../interfaces/reigns.interfaces';
+
+	let step = $state(1);
+	const props: ReignUpsertProps = $props();
+	const maxSteps = 3;
+
+	const nextStep = () => {
+		if (step < maxSteps) {
+			step++;
+		}
+	};
+
+	const previousStep = () => {
+		if (step > 1) {
+			step--;
+		}
+	};
+</script>
+
+<div class="w1 multistep-form-container-external">
+	<div class="w1 multistep-form" style="--maxStep: {maxSteps}; --currentStep: {step}">
+		<div class="w1 multitep-form-step" data-step="1">
+			<Box title="Selección de paso 1" icon="info-circle">
+				<p>Contenido del paso 1</p>
+			</Box>
+		</div>
+		<div class="w1 multitep-form-step" data-step="2">
+			<Box title="Selección de paso 2" icon="info-circle">
+				<p>Contenido del paso 2</p>
+			</Box>
+		</div>
+		<div class="w1 multitep-form-step" data-step="3">
+			<Box title="Selección de paso 3" icon="info-circle">
+				<p>Contenido del paso 3</p>
+			</Box>
+		</div>
+	</div>
+
+	<div class="w1 flex between gap-small multistep-form-buttons-container sidebar-margin">
+		<button type="button" class="btn secondary" onclick={previousStep} disabled={step === 0}>
+			Anterior
+		</button>
+		{#if step >= maxSteps}
+			<button type="submit" class="btn cta">Enviar</button>
+		{:else}
+			<button type="button" class="btn cta" onclick={nextStep} disabled={step === maxSteps}>
+				Siguiente
+			</button>
+		{/if}
+	</div>
+</div>
+
+<style>
+	.multistep-form-container-external {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: space-between;
+		gap: 10px;
+		overflow: hidden;
+	}
+
+	.multistep-form-container-external .multistep-form-buttons-container {
+		width: 100%;
+		position: fixed;
+		bottom: 0;
+		left: 0;
+		padding: 5px 10px;
+	}
+
+	.multistep-form {
+		width: 100%;
+		display: flex;
+		transition: transform 0.3s ease;
+		transform: translateX(calc(-100% * (var(--currentStep) - 1)));
+	}
+
+	.multitep-form-step {
+		flex-shrink: 0;
+		width: 100%;
+		padding: 10px;
+		box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+	}
+</style>
--- src/routes/admin/championship-reigns/create/+page.svelte
+++ src/routes/admin/championship-reigns/create/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
+	import { page } from '$app/state';
 	import AsyncForm from '$lib/components/forms/async-form.svelte';
 	import PageWrapper from '$lib/components/page-wrapper/page-wrapper.svelte';
+	import Multistep from '../components/multistep.svelte';
 	import UpsertReigns from '../components/upsert-reigns.svelte';
 	import type { UpserReignWrestler } from '../interfaces/reigns.interfaces';
 
@@ -10,30 +12,59 @@
 		ppvs: [],
 		teams: []
 	};
+
+	$: isNewDesign = page.url.searchParams.has('new');
 </script>
 
 <PageWrapper page="admin-championship-reigns-update-page">
 	<h1 class="none">Administrar reinados de campeonato</h1>
 
-	<AsyncForm method="post" buttonText="Guardar cambios" redirect="/admin/championship-reigns">
-		<UpsertReigns
-			reign={{} as any}
-			wrestlers={data.wrestlers.map((wrestler) => ({
-				id: wrestler.id,
-				name: wrestler.name,
-				image: wrestler.image_name,
-				gender: wrestler.sex,
-				status: wrestler.status
-			})) as UpserReignWrestler[]}
-			championships={data.championships as {
-				id: number;
-				name: string;
-				image: string;
-				gender: string;
-				tag: boolean;
-			}[]}
-			ppvs={data.ppvs}
-			teams={data.teams}
-		/>
+	<AsyncForm
+		method="post"
+		buttonText="Guardar cambios"
+		redirect="/admin/championship-reigns"
+		showButtons={!isNewDesign}
+	>
+		{#if page.url.searchParams.has('new')}
+			<Multistep
+				reign={{} as any}
+				wrestlers={data.wrestlers.map((wrestler) => ({
+					id: wrestler.id,
+					name: wrestler.name,
+					image: wrestler.image_name,
+					gender: wrestler.sex,
+					status: wrestler.status
+				})) as UpserReignWrestler[]}
+				championships={data.championships as {
+					id: number;
+					name: string;
+					image: string;
+					gender: string;
+					tag: boolean;
+				}[]}
+				ppvs={data.ppvs}
+				teams={data.teams}
+			/>
+		{:else}
+			<UpsertReigns
+				reign={{} as any}
+				wrestlers={data.wrestlers.map((wrestler) => ({
+					id: wrestler.id,
+					name: wrestler.name,
+					image: wrestler.image_name,
+					gender: wrestler.sex,
+					status: wrestler.status
+				})) as UpserReignWrestler[]}
+				championships={data.championships as {
+					id: number;
+					name: string;
+					image: string;
+					gender: string;
+					tag: boolean;
+				}[]}
+				ppvs={data.ppvs}
+				teams={data.teams}
+			/>
+		{/if}
 	</AsyncForm>
 </PageWrapper>
