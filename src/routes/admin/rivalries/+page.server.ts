d8a3b03dbf55dcf41c5a851bcb25025dcedd33ca
DavidFFDAW <davidferflo2@gmail.com>
2025-08-26 00:45:22 +0200

    feat: rivalry upsert page

--- src/routes/admin/rivalries/+page.server.ts
+++ src/routes/admin/rivalries/+page.server.ts
@@ -1,17 +1,30 @@
 import { RivalriesRepository } from '$lib/server/dao/repositories/rivalries.repository.js';
 import { TeamsRepository } from '$lib/server/dao/repositories/teams.repository';
+import { WrestlerRepository } from '$lib/server/dao/repositories/wrestler.repository';
 
 export async function load() {
 	const rivalriesRepository = new RivalriesRepository();
 	const teamsRepository = new TeamsRepository();
+	const wrestlerRepository = new WrestlerRepository();
 
-	const teams = await teamsRepository.getTeamsWithMembers();
+	const wrestlers = (
+		await wrestlerRepository.getNonReleasedWrestlers({
+			select: { name: true }
+		})
+	).map((wrestler) => wrestler.name);
+	const teams = await teamsRepository.getTeamsWithMembers({
+		where: {
+			active: true
+		}
+	});
 	const teamNames = teams.map((team) => {
 		if (team.WrestlerTeam.length > 2) {
-			return team.WrestlerTeam.map((wt) => wt.Wrestler.name).join(', ');
+			return team.name + ' (' + team.WrestlerTeam.map((wt) => wt.Wrestler.name).join(', ') + ')';
 		}
 		return team.name;
 	});
 
-	return { rivalries: await rivalriesRepository.get(), teams: teamNames };
+	return { rivalries: await rivalriesRepository.get(), teams: teamNames, wrestlers };
 }
+
+export const actions = {};
--- src/routes/admin/rivalries/+page.svelte
+++ src/routes/admin/rivalries/+page.svelte
@@ -1,5 +1,17 @@
 <script lang="ts">
-	import Debug from '$lib/components/visual/debug.svelte';
+	import AsyncForm from '$lib/components/forms/async-form.svelte';
+	import BrandsSelector from '$lib/components/forms/inputs/brands-selector.svelte';
+	import type { Rivalries } from '@prisma/client';
+
+	let rivalryData: Rivalries = {
+		id: 0,
+		first_rival: '',
+		second_rival: '',
+		intensity: 'high',
+		brand: '',
+		created_at: null,
+		updated_at: null
+	};
 	export let data;
 </script>
 
@@ -9,7 +21,20 @@
 		<small>Consulta aquí tus rivalidades actuales</small>
 	</header>
 
-	<Debug datas={data} />
+	<AsyncForm method="post" action="" showButtons={false} updateId={rivalryData.id}>
+		<input type="hidden" name="_action" value={rivalryData.id ? 'update' : 'create'} />
+
+		<div class="form-datas">
+			<p>Primer participante</p>
+			<p>Segundo participante</p>
+			<p>Intensidad</p>
+			<BrandsSelector bind:value={rivalryData.brand} />
+		</div>
+
+		<div class="w1 flex end">
+			<button type="submit">Crear Rivalidad</button>
+		</div>
+	</AsyncForm>
 
 	<ul class="rivalries-list">
 		{#each data.rivalries as rivalry}
