696c0dc5a2cf729c14d26e5edeaa4ed21d7d073a
DavidFFDAW <davidferflo2@gmail.com>
2025-09-04 13:21:26 +0200

    feat: new design for championship create page

--- src/routes/admin/championship-reigns/create/new/+page.svelte
+++ src/routes/admin/championship-reigns/create/new/+page.svelte
@@ -18,6 +18,8 @@
 		ppvs: [],
 		teams: []
 	};
+	const initialWrestlersList = data.wrestlers;
+	const initialChampionshipsList = data.championships;
 
 	const scrollIntoActiveStep = () => {
 		const activeStep = document.querySelector('.step.active');
@@ -48,112 +50,230 @@
 		image: string;
 	} | null = null;
 	let selectedWrestler: Wrestler | null = null;
+
+	const searchItem =
+		(type: 'wrestlers' | 'championships', initialList: any[]) => (event: Event) => {
+			const input = event.target as HTMLInputElement;
+			const query = input.value.toLowerCase();
+
+			if (!query) {
+				data[type] = initialList;
+				return;
+			}
+
+			data[type] = data[type].filter((item) => item.name?.toLowerCase().includes(query)) as any[];
+		};
+
+	const selectItem = (type: 'wrestlers' | 'championships', item: any) => {
+		if (type === 'wrestlers') {
+			selectedWrestler = item;
+		} else if (type === 'championships') {
+			selectedChampionship = item;
+		}
+		nextStep();
+	};
 </script>
 
 <PageWrapper page="admin-championship-reigns-update-page">
 	<AsyncForm method="post" buttonText="Guardar cambios" showButtons={false}>
-		<div class="steps">
-			<div class="step" class:active={step === 1}>
-				<h2>Seleccionar campeonato</h2>
-				<div class="wrestler-selector">
-					<ResourceSelector
-						name="championship_id"
-						list={data.championships.map((championship) => ({
-							id: championship.id,
-							name: championship.name as string,
-							image: championship.image as string
-						}))}
-						bind:selectedResource={selectedChampionship}
-						afterSelection={(selected) => {
-							if (selected) nextStep();
-						}}
-					/>
-				</div>
-			</div>
-			<div class="step" class:active={step === 2}>
-				<h2>Seleccionar luchador</h2>
-				<div class="wrestler-selector">
-					<WrestlersSelector
-						name="wrestler_id"
-						list={data.wrestlers}
-						afterSelection={(selected) => {
-							if (selected) nextStep();
-						}}
-						bind:selectedWrestler
-					/>
-				</div>
-			</div>
-			<div class="step" class:active={step === 3}>
-				<div class="ppv-selector">
-					<UpsertCommonDatas ppvs={data.ppvs} isUpdate={false} />
-					<PpvSelector name="ppv-selector-picker" ppvs={data.ppvs} />
-				</div>
-			</div>
-		</div>
+		<div class="form-wrapper" style="--currentStep: {step}; --maxSteps: {maxSteps}">
+			<div class="w1 form-items form-steps">
+				<section class="form-step step-1 form-custom-step" class:active={step === 1}>
+					<h2 class="w1 form-step-title">
+						<i class="bi bi-award icon-lg"></i>
+						<span>Seleccionar campeonato</span>
+					</h2>
+					<div class="form-step-content fancy-scroll" transition:fade>
+						<header class="w1 form-step-header">
+							<input
+								type="search"
+								placeholder="Buscar luchador..."
+								class="w1 searcher-input"
+								on:input|preventDefault={searchItem('championships', initialChampionshipsList)}
+							/>
+						</header>
 
-		<div class="step-buttons">
-			<button type="button" on:click={previousStep} disabled={step === 1}>
-				<Icon icon="arrow-left" classes="icon-big" />
-				<span class="btn-text">Anterior</span>
-			</button>
+						<ul class="grid grid-two-column gap-smaller down">
+							{#each data.championships as championship}
+								<li class="championship-option-item">
+									<label class="relative">
+										<input
+											type="radio"
+											name="championship"
+											class="app-radio"
+											value={championship.id}
+											bind:group={selectedChampionship}
+											on:change={nextStep}
+										/>
+										<div class="inner championship-card">
+											<Image
+												src={championship.image}
+												alt={championship.name as string}
+												class="championship-image"
+												width={50}
+												height={50}
+												placeholder="blur"
+											/>
+											<span class="championship-name">{championship.name}</span>
+										</div>
+									</label>
+								</li>
+							{/each}
+						</ul>
+					</div>
+				</section>
 
-			<button type={step < maxSteps ? 'button' : 'submit'} on:click={nextStep} transition:fade>
-				{#if step < maxSteps}
-					<Icon icon="arrow-right" classes="icon-big" />
-				{:else}
-					<Icon icon="check" classes="icon-big" />
-				{/if}
-				<span class="btn-text">{step < maxSteps ? 'Siguiente' : 'Guardar'}</span>
-			</button>
+				<section class="form-step step-2 form-custom-step" class:active={step === 2}>
+					<h2 class="w1 form-step-title">
+						<i class="bi bi-person icon-lg"></i>
+						<span>Seleccionar luchador</span>
+					</h2>
+					<div class="form-step-content fancy-scroll" transition:fade>
+						<header class="w1 form-step-header">
+							<input
+								type="search"
+								placeholder="Buscar luchador..."
+								class="w1 searcher-input"
+								on:input={searchItem('wrestlers', initialWrestlersList)}
+							/>
+						</header>
+
+						<ul class="grid grid-two-column gap-smaller down">
+							{#each data.wrestlers as wrestler}
+								<li class="championship-option-item">
+									<label class="relative">
+										<input
+											type="radio"
+											name="championship"
+											class="app-radio"
+											value={wrestler.id}
+											bind:group={selectedWrestler}
+											on:change={nextStep}
+										/>
+										<div class="inner championship-card">
+											<Image
+												src={wrestler.image_name as string}
+												alt={wrestler.name as string}
+												class="championship-image"
+												width={50}
+												height={50}
+												placeholder="blur"
+											/>
+											<span class="championship-name">{wrestler.name}</span>
+										</div>
+									</label>
+								</li>
+							{/each}
+						</ul>
+					</div>
+				</section>
+			</div>
+			<div class="w1 form-buttons flex between">
+				<button
+					type="button"
+					class="btn secondary"
+					aria-label="Anterior"
+					on:click={previousStep}
+					disabled={step === 1}
+				>
+					<i class="bi bi-arrow-left"></i>
+				</button>
+
+				<span>Paso {step} de {maxSteps}</span>
+
+				<button
+					type={step === maxSteps + 1 ? 'submit' : 'button'}
+					class="btn cta"
+					aria-label={step === maxSteps ? 'Finalizar' : 'Siguiente'}
+					on:click={nextStep}
+					disabled={step === maxSteps + 1}
+				>
+					<i class="bi bi-{step === maxSteps ? 'check' : 'arrow-right'}"></i>
+				</button>
+			</div>
 		</div>
 	</AsyncForm>
 </PageWrapper>
 
 <style>
-	.wrestler-selector {
+	.form-wrapper {
 		width: 100%;
-		overflow: hidden;
-	}
-	.steps {
 		display: flex;
-		gap: 1rem;
-		margin-bottom: 2rem;
-		min-height: 70vh;
-		max-height: 70vh;
-		padding: 1rem;
-		background-color: #f9f9f9;
-		border-radius: 10px;
-	}
-
-	.step {
-		display: none;
-		flex: 1;
-	}
-
-	.step.active {
-		display: block;
-	}
-
-	.step-buttons {
-		display: flex;
+		flex-direction: column;
 		justify-content: space-between;
-		margin-top: 2rem;
+		padding: 15px;
+		border-radius: 10px;
+		border: 1px solid #ddd;
+		min-height: calc(100dvh - 45px - 15px); /* Ajusta si es necesario */
+		background-color: #fff;
+		box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+		overflow: hidden; /* ¡Crucial para que los pasos fuera de vista no se salgan! */
 	}
 
-	.step-buttons button {
-		padding: 0.5rem 1rem;
-		background-color: #007bff;
-		color: white;
-		border: none;
-		border-radius: 5px;
+	.form-wrapper .form-items {
+		display: flex;
+		width: 100%;
+		/* width: calc(100% * var(--maxSteps)); */
+		height: 100%;
+		min-height: 100%; /* Ajusta según sea necesario */
+		/* Agregamos una transición para que el movimiento sea suave */
+		transition: transform 0.5s ease-out;
+		/* La transformación que mueve el contenedor completo */
+		/* transform: translateX(calc(-100% * (var(--currentStep) - 1))); */
+		gap: 15px;
+	}
+
+	.form-wrapper .form-items .form-step {
+		display: none;
+		width: 100%;
+		height: 100%;
+		box-sizing: border-box;
+	}
+	.form-wrapper .form-items .form-step.active {
+		display: block; /* Muestra solo el paso activo */
+	}
+
+	/* Estilos para el contenido dentro de cada paso */
+	.form-wrapper .form-items .form-step .form-step-title {
+		text-align: center;
+		text-transform: uppercase;
+		font-size: 1.5em;
+		color: #333;
+	}
+	.form-wrapper .form-items .form-step .form-step-title i.bi {
+		margin-right: 5px;
+	}
+
+	.form-step-content {
+		width: 100%;
+		padding: 0 12px;
+		padding-top: 15px;
+		flex-grow: 1;
+		min-height: calc(100dvh - 195px); /* Ajusta según la altura del título */
+		max-height: calc(100dvh - 195px); /* Ajusta según la altura del título y los botones */
+		overflow: hidden auto;
+	}
+
+	.championship-card {
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		gap: 2px;
+		padding: 10px 8px;
+		border-radius: 8px;
+		background-color: #fff;
+		border: 1px solid #ccc;
+		box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 		cursor: pointer;
 	}
 
-	.ppv-selector {
-		width: 100%;
-		max-width: 300px;
-		max-height: 400px;
-		overflow: hidden;
-		overflow-y: auto;
+	.championship-card .championship-name {
+		margin-top: 5px;
+		font-weight: bold;
+		text-align: center;
+		font-size: 0.9em;
+		color: #333;
 	}
 </style>
