d9f5ee9d2a42f55a518aa0038271cd23dbb86efc
DavidFFDAW <davidferflo2@gmail.com>
2026-04-11 18:12:30 +0200

    feat: added matchcard summary blog intetgration

--- src/routes/admin/matchcards/summary/+page.server.ts
+++ src/routes/admin/matchcards/summary/+page.server.ts
@@ -1,5 +1,8 @@
 import { redirect } from '@sveltejs/kit';
 import { PpvCardRepository } from '$lib/server/dao/repositories/matchcard.repository.js';
+import { Helpers } from '$lib/server/server.helpers.js';
+import { BlogRepository } from '$lib/server/dao/repositories/blog.repository.js';
+import { Utils } from '$lib/utils/general.utils.js';
 
 export const load = async ({ url }) => {
 	const id = url.searchParams.get('id');
@@ -30,21 +33,90 @@ export const load = async ({ url }) => {
 	const matches = 'matches' in card_data ? (card_data.matches as any[]) : [];
 	const groupedByNight: Record<string, any[]> = matches.reduce((acc, match) => {
 		const night = match.night || 1;
+		match.notes = '';
 		if (!acc[night]) {
 			acc[night] = [];
 		}
 		acc[night].push(match);
 		return acc;
 	}, {});
+	const nights = Object.keys(groupedByNight).map((night) => Number(night));
 
 	return {
 		ppv: {
 			id: card_data.id,
 			name: card_data.ppv_name,
 			image: card_data.ppv_image,
-			date: card_data.ppv_date
+			date: card_data.ppv_date,
+			post: card_data.post_id
 		},
+		hasLinkedPost: Boolean(card_data.post_id),
+		hasMultipleNights: nights.length > 1,
 		matches: groupedByNight,
-		nights: Object.keys(groupedByNight).map((night) => Number(night))
+		nights: nights
 	};
+};
+
+export const actions = {
+	default: async ({ request, locals, url }) => {
+		try {
+			if (!Helpers.hasPermission(locals))
+				return Helpers.error('No tienes permiso para realizar esta acción', 403);
+
+			const urlId = Number(url.searchParams.get('id'));
+			const formData = await request.formData();
+			const action = formData.get('action');
+			const updateId = Helpers.getUpdateID(formData);
+
+			if (!urlId || isNaN(urlId)) return Helpers.error('ID de matchcard no válido', 400);
+			if (!updateId || isNaN(updateId)) return Helpers.error('ID de actualización no válido', 400);
+			if (updateId !== urlId)
+				return Helpers.error('ID de actualización no coincide con el de la URL', 400);
+
+			if (action !== 'create_summary') return Helpers.error('Acción no soportada', 400);
+
+			const { error, message } = Helpers.checkRequiredFields(formData, [
+				'name',
+				'date',
+				'image',
+				'content'
+			]);
+			if (error) return Helpers.error(message, 400);
+
+			const datas = {
+				name: formData.get('name') as string,
+				date: new Date(formData.get('date') as string),
+				image: formData.get('image') as string,
+				content: formData.get('content') as string
+			};
+			const title = `Resumen de ${datas.name}`;
+			const author = locals.user?.uuid ? Number(locals.user?.uuid) : 0;
+
+			const postRepository = new BlogRepository();
+			const ppvCardRepository = new PpvCardRepository();
+			const createdPost = await postRepository.create({
+				title: title.trim(),
+				slug: Helpers.slugify(title),
+				content: datas.content.trim(),
+				status: 'published',
+				created_at: datas.date,
+				category: 'summary',
+				image: datas.image.trim(),
+				visible: true,
+				exceptr: `Resumen del evento ${datas.name}, celebrado el ${Utils.getDateLocale(datas.date)}. En este post encontrarás un resumen de lo acontecido en el evento.`,
+				admin: { connect: { id: author } }
+			});
+
+			if (!createdPost) return Helpers.error('No se pudo crear el post', 500);
+
+			await ppvCardRepository.updateById(urlId, {
+				post_id: createdPost.id
+			});
+
+			return Helpers.success('Post creado correctamente', 201);
+		} catch (err) {
+			console.error('Error processing form data:', err);
+			return Helpers.error('Error al procesar los datos del formulario', 500);
+		}
+	}
 };
--- src/routes/admin/matchcards/summary/+page.svelte
+++ src/routes/admin/matchcards/summary/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+	import AsyncForm from '$lib/components/forms/async-form.svelte';
+	import QuillInput from '$lib/components/forms/inputs/quill-input.svelte';
 	import { Toast } from '$lib/utils/toast.helper';
+
 	let { data } = $props();
 	let selectedNight = $state(data.nights[0] || 1);
 	let matches = $derived(data.matches[selectedNight]);
@@ -11,11 +14,11 @@
 				if (item.championship) text += `\n - Championship: ${item.championship}`;
 				if (item.winner) text += `\n - Winner: ${item.winner}`;
 				if (item.stipulation) text += `\n - Stipulation: ${item.stipulation}`;
-				text += `\n - Notes:`;
+				if (item.notes) text += `\n - Notes: ${item.notes}`;
 				return text.trim();
 			})
 			.filter(Boolean)
-			.join('\n');
+			.join('\n\n');
 
 		try {
 			await navigator.clipboard.writeText(textContent || '');
@@ -24,17 +27,15 @@
 			Toast.error('Error al copiar al portapapeles');
 		}
 	};
-
-	$inspect(data);
 </script>
 
 <header class="page-header">
 	<h1>Resumen de matchcards</h1>
-	<small>Aquí puedes ver un resumen de esta matchcard.</small>
+	<small>Aquí puedes ver un resumen de esta matchcard</small>
 </header>
 
 <section class="page-container">
-	{#if data.nights.length > 1}
+	{#if data.hasMultipleNights}
 		<header class="w1 nights-container">
 			{#each data.nights as night}
 				<label class="form-label relative">
@@ -46,7 +47,7 @@
 	{/if}
 
 	<div class="w1 page-summary-list flex start astart column gap-5 copiable">
-		<h2>Noche {selectedNight}</h2>
+		<h2 class="uppercase">Noche {selectedNight}</h2>
 
 		{#each matches as match}
 			<div class="w1 page-summary-item box gap-smaller">
@@ -58,41 +59,113 @@
 				{#if match.winner}
 					<p class="winner">- Ganador: {match.winner}</p>
 				{/if}
-				<p class="night">- Noche: {match.night}</p>
+				<label class="form-item">
+					<span class="form-label label">Notas:</span>
+					<textarea class="notes" placeholder="Notas del combate..." bind:value={match.notes}
+					></textarea>
+				</label>
 			</div>
 		{/each}
 	</div>
 
-	<div class="w1 buttons flex total astart responsive gap-smaller">
-		<button class="btn rounded cta icon" type="button" onclick={copyToClipboard}>
-			<i class="bi bi-clipboard"></i>
-			<span>Copiar</span>
-		</button>
-
-		<a
-			class="btn rounded cta icon"
-			href="https://chatgpt.com/g/g-p-67aaf80f33908191bb5ffab06d85b978-wwe/c/9b2d8a04-cd8a-4fb1-9e0f-f76bc23a0479"
-			target="_blank"
-			rel="noopener noreferrer"
-		>
-			<i class="bi bi-chat-dots"></i>
-			Resume con ChatGPT
+	{#if data.ppv.post && data.hasLinkedPost}
+		<a href={`/admin/blog/edit/${data.ppv.post}`} class="post-link btn icon rounded secondary cta">
+			<i class="bi bi-file-earmark-text"></i>
+			<span>Editar post relacionado</span>
 		</a>
+	{:else}
+		<AsyncForm method="post" showButtons={false} redirect="/admin/blog" updateId={data.ppv.id}>
+			<input type="hidden" name="date" value={data.ppv.date?.toISOString().split('T')[0]} />
+			<input type="hidden" name="image" value={data.ppv.image} />
+			<input type="hidden" name="name" value={data.ppv.name} />
+			<input type="hidden" name="action" value="create_summary" />
 
-		<a href="/admin/blog/create" class="btn rounded cta icon">
-			<i class="bi bi-file-earmark-plus"></i>
-			Crear un nuevo post
-		</a>
-	</div>
+			<div class="w1 box create-summary-post">
+				<header class="title-block flex start acenter gap-smaller pointer">
+					<i class="bi bi-card-text text-2xl"></i>
+					<h2 class="box-title">Crear resumen</h2>
+				</header>
+
+				<div class="post-info">
+					<p>
+						Para crear un post relacionado con esta matchcard, recomendamos pulsar en el botón de
+						"copiar" para copiar el contenido de la matchcard y después acceder a <a
+							href="https://chatgpt.com/g/g-p-67aaf80f33908191bb5ffab06d85b978-wwe/c/9b2d8a04-cd8a-4fb1-9e0f-f76bc23a0479"
+							class="link"
+							target="_blank"
+							rel="noopener noreferrer">ChatGPT</a
+						> para generar un resumen del evento. Una vez tengas el resumen, pégalo en el campo de contenido
+						y pulsa en "Crear post".
+					</p>
+
+					<div class="w1 buttons flex total astart responsive gap-smaller">
+						<button class="btn rounded cta icon" type="button" onclick={copyToClipboard}>
+							<i class="bi bi-clipboard"></i>
+							<span>Copiar</span>
+						</button>
+
+						<a
+							class="btn rounded icon chatgpt-btn"
+							href="https://chatgpt.com/g/g-p-67aaf80f33908191bb5ffab06d85b978-wwe/c/9b2d8a04-cd8a-4fb1-9e0f-f76bc23a0479"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<i class="bi bi-chat-dots"></i>
+							Accede a ChatGPT
+						</a>
+					</div>
+
+					<QuillInput
+						label="Contenido"
+						name="content"
+						placeholder="Escribe aquí el contenido del post relacionado con esta matchcard..."
+						value=""
+						required
+					/>
+					<div class="w1 flex down between acenter gap-smaller">
+						<button class="btn rounded secondary" type="reset">Cancelar</button>
+						<button class="btn rounded cta" type="submit">Crear post</button>
+					</div>
+				</div>
+			</div>
+		</AsyncForm>
+	{/if}
 </section>
 
 <style>
-	.flex.buttons {
-		margin-top: 25px;
+	.page-container {
+		display: flex;
+		flex-direction: column;
+		gap: 20px;
+	}
+	.page-summary-item label.form-item {
+		margin-top: 10px;
+		gap: 2px;
+	}
+	.page-summary-item label.form-item textarea {
+		width: 100%;
+		min-height: 50px;
+		padding: 8px;
+		border: 1px solid #ccc;
+		border-radius: 4px;
+		resize: vertical;
+	}
+	.create-summary-post .post-info {
+		display: flex;
+		padding: 10px 0;
+		flex-direction: column;
+		align-items: start;
+		justify-content: start;
+		margin-top: 10px;
+		gap: 20px;
 	}
 	.flex.buttons > * {
 		flex: 1;
 		width: 100%;
+	}
+	a.link {
+		color: #193453;
+		text-decoration: underline;
 	}
 	header.nights-container {
 		margin-bottom: 20px;
@@ -117,6 +190,20 @@
 	header.nights-container label input:checked + .label.inner {
 		background-color: #333;
 		border-color: #333;
+		color: #fff;
+	}
+
+	.buttons.flex > .btn {
+		padding: 6px 10px;
+		font-size: 0.9rem;
+		background-color: #55759a;
+	}
+	.buttons.flex > .btn.chatgpt-btn {
+		min-width: 200px;
+		text-align: center;
+		border-radius: 50px;
+		background-color: #19a974;
+		border-color: #19a974;
 		color: #fff;
 	}
 </style>
