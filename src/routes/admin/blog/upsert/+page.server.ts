da91131aedbd3a78d08fdc3ffaeb087a708707fd
DavidFFDAW <davidferflo2@gmail.com>
2026-04-16 15:21:25 +0200

    feat: upsert blog post page progress

--- src/routes/admin/blog/upsert/+page.server.ts
+++ src/routes/admin/blog/upsert/+page.server.ts
@@ -1,5 +1,6 @@
 import { BlogRepository } from '$lib/server/dao/repositories/blog.repository.js';
 import { UsersRepository } from '$lib/server/dao/repositories/users.repository.js';
+import { Helpers } from '$lib/server/server.helpers.js';
 import type { Prisma } from '@prisma/client';
 
 const getPost = async (id: number, repository: BlogRepository) => {
@@ -20,6 +21,7 @@ export const load = async ({ url, locals }) => {
 	const id = Number(url.searchParams.get('id'));
 	const post = await getPost(id, repository);
 	const isUpdate = Boolean(post.id);
+	const maxMinViews = await repository.getMaxMinViewsRange();
 
 	const authors = await users.get({
 		select: {
@@ -34,8 +36,38 @@ export const load = async ({ url, locals }) => {
 			post,
 			authors,
 			isUpdate,
+			maxMinViews,
 			action: isUpdate ? 'update' : 'create',
 			post_author: post.admin_id ? post.admin_id : 0,
 		}
 	};
+}
+
+export const actions = {
+	default: async ({ request, url }) => {
+		const datas = await request.formData();
+		const id = Number(url.searchParams.get('id'));
+		if (!id || isNaN(id)) return Helpers.error('ID de post no válido', 400);
+		
+		const updateId = Helpers.getUpdateID(datas);
+		if (!updateId || isNaN(updateId)) return Helpers.error('ID de actualización no válido', 400);
+		if (updateId !== id) return Helpers.error('ID de actualización no coincide con el ID del post', 400);
+
+		const post: Prisma.BlogPostCreateInput = {
+			title: datas.get('title') as string,
+			slug: datas.get('slug') as string,
+			exceptr: datas.get('excerpt') as string,
+			content: datas.get('content') as string,
+			views: Number(datas.get('views') || 0),
+			visible: datas.get('visible') === 'visible',
+			deletable: datas.get('autodelete') === 'deletable',
+			category: datas.get('category') as string,
+			status: datas.get('status') as string,
+			image: datas.get('image') as string,
+			admin: { connect: { id: Number(datas.get('post_author')) } }
+		};
+		console.log({ post, updateId, autodelete: datas.get('autodelete') });
+
+		return Helpers.error('No se ha especificado una acción válida', 400);
+	 }
 }
--- src/routes/admin/blog/upsert/+page.svelte
+++ src/routes/admin/blog/upsert/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import AsyncForm from '$lib/components/forms/async-form.svelte';
 	import { Utils } from '$lib/utils/general.utils.js';
 	import { slide } from 'svelte/transition';
 	// import { tick } from 'svelte';
@@ -6,6 +7,8 @@
 	let { data } = $props();
 	let post = $state(data.upsert.post);
 	let slug = $derived(Utils.slugify(post?.title || ''));
+
+	let images = $state<{ url: string; name: string }[]>([]);
 	let showGenerateIA = $state(false);
 	// let blockAiDiv = $state<HTMLDivElement | null>(null);
 
@@ -16,6 +19,29 @@
 	const toggleClickIA = () => {
 		showGenerateIA = !showGenerateIA;
 	};
+
+	const handleGetImages = async (event: Event) => {
+		event.preventDefault();
+		const form = event.target as HTMLFormElement;
+		const formData = new FormData(form);
+		const query = formData.get('image_query') as string;
+		if (!query) return;
+
+		try {
+			const response = await fetch(
+				`/api/images?provider=google&search=${encodeURIComponent(query)}`
+			);
+			if (!response.ok) throw new Error('Error al buscar imágenes');
+
+			const data = await response.json();
+			console.log({ data });
+			images = data.images; // Asegúrate de que la respuesta tenga esta estructura
+		} catch (error) {
+			console.error('Error al obtener imágenes:', error);
+		}
+	};
+
+	$inspect(data.upsert);
 </script>
 
 <button type="button" class="w1 btn icon cta" onclick={toggleClickIA}>
@@ -23,40 +49,114 @@
 	<span>Abrir bloque de generación con IA</span>
 </button>
 
-<div class="w1 box flex column gap-smaller blog-upsert-datas down">
-	<div class="w1">
-		<input type="text" bind:value={post.title} placeholder="Título del post" />
-		<small class="text-muted">{slug}</small>
+<div class="page-container grid grid-page-layout responsive">
+	<AsyncForm
+		method="post"
+		updateId={post.id}
+		showButtons={false}
+		classes="w1 form-upsert-blog-post"
+	>
+		<div class="w1 box flex column gap-smaller blog-upsert-datas">
+			<div class="w1">
+				<input type="text" name="title" bind:value={post.title} placeholder="Título del post" />
+				<input type="hidden" name="slug" value={slug} />
+				<small class="text-muted">{slug}</small>
+			</div>
+
+			<input type="text" name="excerpt" value={post.exceptr} placeholder="Extracto del post" />
+			<textarea bind:value={post.content} name="content" placeholder="Contenido del post"
+			></textarea>
+
+			<input type="number" name="views" value={post.views} placeholder="Número de vistas" />
+
+			<label>
+				<span>Visible</span>
+				<input type="checkbox" name="visible" checked={post.visible} value="visible" />
+			</label>
+
+			<label class="relative">
+				<input
+					type="checkbox"
+					class="app-radio app-checkbox"
+					name="autodelete"
+					bind:checked={post.deletable}
+					value="deletable"
+				/>
+				<span class="radio-check-inner inner">Autoborrado</span>
+			</label>
+
+			<input type="text" name="category" value={post.category} placeholder="Categoría del post" />
+			<input type="text" name="status" value={post.status} placeholder="Estado del post" />
+			<input type="text" name="image" value={post.image} placeholder="Imagen del post" />
+
+			<select name="author" value={data.upsert.post_author}>
+				<option value={0} disabled>Selecciona un autor</option>
+				{#each data.upsert.authors as author}
+					<option value={author.id}>{author.name}</option>
+				{/each}
+			</select>
+
+			<div class="w1 flex between gap-5 buttons-container-item">
+				<button type="reset" class="btn icon secondary icon-gap-5">
+					<i class="bi bi-x-lg"></i>
+					<span>Cancelar</span>
+				</button>
+				<button type="submit" class="btn icon cta icon-gap-5">
+					<i class="bi bi-check-lg"></i>
+					<span>Guardar</span>
+				</button>
+			</div>
+		</div>
+	</AsyncForm>
+
+	{#if showGenerateIA}
+		<div class="w1 box blog-ia-generate-block" transition:slide={{ duration: 120 }}>
+			<p>
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil soluta doloremque eveniet
+				incidunt placeat impedit voluptate, vitae voluptas? Laborum alias officiis sunt cupiditate?
+				Ea quidem voluptas corrupti eos facere quae.
+			</p>
+		</div>
+	{/if}
+
+	<div class="w1 box blog-images-box">
+		<form action="" onsubmit={handleGetImages}>
+			<input type="text" name="image_query" placeholder="Buscar imágenes" />
+			<button type="submit" class="btn icon" aria-label="Buscar imágenes">
+				<i class="bi bi-search"></i>
+			</button>
+		</form>
+
+		<ul class="w1 images-list">
+			{#each images as image}
+				<li>
+					<img src={image.url} alt={image.name} />
+					<span>{image.name}</span>
+				</li>
+			{/each}
+		</ul>
 	</div>
-
-	<input type="text" value={post.exceptr} placeholder="Extracto del post" />
-	<textarea bind:value={post.content} placeholder="Contenido del post"></textarea>
-
-	<input type="number" name="views" value={post.views} placeholder="Número de vistas" />
-
-	<label>
-		<span>Visible</span>
-		<input type="checkbox" name="visible" checked={post.visible} />
-	</label>
-
-	<input type="text" name="category" value={post.category} placeholder="Categoría del post" />
-	<input type="text" name="status" value={post.status} placeholder="Estado del post" />
-	<input type="text" name="image" value={post.image} placeholder="Imagen del post" />
-
-	<select name="author" value={data.upsert.post_author}>
-		<option value={0} disabled>Selecciona un autor</option>
-		{#each data.upsert.authors as author}
-			<option value={author.id}>{author.name}</option>
-		{/each}
-	</select>
 </div>
 
-{#if showGenerateIA}
-	<div class="w1 box blog-ia-generate-block down" transition:slide={{ duration: 120 }}>
-		<p>
-			Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil soluta doloremque eveniet
-			incidunt placeat impedit voluptate, vitae voluptas? Laborum alias officiis sunt cupiditate? Ea
-			quidem voluptas corrupti eos facere quae.
-		</p>
-	</div>
-{/if}
+<style>
+	.grid.grid-page-layout {
+		margin-top: 1rem;
+		grid-template-columns: repeat(2, 1fr);
+		gap: 1rem;
+	}
+
+	.app-checkbox + .inner.radio-check-inner {
+		color: #d32f2f;
+		border: 1px solid #d32f2f;
+		border-radius: 50px;
+		padding: 4px 20px;
+		box-shadow: none;
+		outline: none;
+	}
+	.app-checkbox:checked + .inner.radio-check-inner {
+		color: #fff;
+		box-shadow: none;
+		background-color: #d32f2f;
+		border: 1px solid #d32f2f;
+	}
+</style>
