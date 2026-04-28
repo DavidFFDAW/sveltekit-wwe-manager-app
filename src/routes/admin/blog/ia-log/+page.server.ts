9b05d810949e3cc23b6cd391134784f794c400bc
DavidFFDAW <davidferflo2@gmail.com>
2026-04-28 15:33:34 +0200

    feat: enhance in ia-logs page

--- src/routes/admin/blog/ia-log/+page.server.ts
+++ src/routes/admin/blog/ia-log/+page.server.ts
@@ -1,4 +1,3 @@
-import { Helpers } from '$lib/server/server.helpers';
 import { IaRepository } from '$lib/server/dao/repositories/ia.repository.js'
 
 type LogEntry = {
@@ -8,26 +7,30 @@ type LogEntry = {
 }
 
 
-export const load = async ({ url }) => { 
+export const load = async () => { 
 	const logs = new IaRepository();
-	const searchPagination = Helpers.getPaginationFromSearchParams(url.searchParams);
-	const connection = await logs.conn();
+	const connection = logs.conn();
 	
 	const today = new Date();
 	const start = today.toISOString().slice(0, 10) + ' 00:00:00';
 	const end = today.toISOString().slice(0, 10) + ' 23:59:59';
-	const dates: string[] = await connection.$queryRaw`SELECT DISTINCT(DATE(created_at)) AS dates FROM ia_log ORDER BY created_at DESC`;
+
+	const dates: { dates: Date }[] = await connection.$queryRaw`SELECT DISTINCT(DATE(created_at)) AS dates FROM ia_log ORDER BY created_at DESC`;
 	const usage: LogEntry[] = await connection.$queryRaw`SELECT model, DATE(created_at) AS \`date\`, SUM(total_tokens) AS tokens FROM ia_log WHERE created_at > ${start} AND created_at < ${end} GROUP BY model, DATE(created_at) ORDER BY created_at DESC, model ASC`;
+	
 	const cleanedUsage = usage.map((entry: any) => ({
 		model: entry.model,
 		date: entry.date.toISOString().slice(0, 10),
 		tokens: Number(entry.tokens)
 	})) as LogEntry[];
 
+	const cleanedDates = dates.map(d => d.dates.toLocaleString('es-ES', { day: '2-digit', month: '2-digit', year: 'numeric' }));
+
 	return {
 		ia: {
-			dates: dates.map((d: any) => d.dates.toISOString().slice(0, 10)),
 			usage: cleanedUsage,
+			dates: cleanedDates,
+			currentDate: today.toLocaleString('es-ES', { day: '2-digit', month: '2-digit', year: 'numeric' })
 		}
 	}
 }
--- src/routes/admin/blog/ia-log/+page.svelte
+++ src/routes/admin/blog/ia-log/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import { Utils } from '$lib/utils/general.utils.js';
 	let { data } = $props();
 </script>
 
@@ -7,6 +6,18 @@
 	<header class="page-header">
 		<h1 class="page-title">Logs de IA</h1>
 	</header>
+
+	<a href="https://console.groq.com/docs/rate-limits" class="btn button groq-button"
+		>Revisar rate limits de Groq
+	</a>
+
+	<div class="dates flex start acenter gap-5 dates-container overflow-horizontal">
+		{#each data.ia.dates as date}
+			<button class="btn btn-filter" type="button" class:active={data.ia.currentDate == date}
+				>{date}</button
+			>
+		{/each}
+	</div>
 
 	<pre>
 		{JSON.stringify(data.ia, null, 4)}
@@ -27,3 +38,16 @@
 		{/each} -->
 	</ul>
 </div>
+
+<style>
+	.btn.groq-button {
+		display: inline-block;
+		color: #fff;
+		background-color: #ff3e00;
+		padding: 3px 20px;
+		font-size: 0.85em;
+		border-radius: 50px;
+		text-decoration: none;
+		font-weight: bold;
+	}
+</style>
