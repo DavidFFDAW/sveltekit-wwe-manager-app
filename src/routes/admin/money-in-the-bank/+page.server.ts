cf978e8a55bc6db16b53a84c4075e223aab57ceb
DavidFFDAW <davidferflo2@gmail.com>
2026-05-05 13:00:41 +0200

    fix: mitb page error

--- src/routes/admin/money-in-the-bank/+page.server.ts
+++ src/routes/admin/money-in-the-bank/+page.server.ts
@@ -1,33 +1,45 @@
 import { ReignsRepository } from "$lib/server/dao/repositories/reigns.repository";
 
 export const load = async ({ url }) => {
-	const filterYear = url.searchParams.get('year');
-	const currentYear = filterYear ? Number(filterYear) : new Date().getFullYear();
+	const filterYear = url.searchParams.get('year') as string | null;
+	const currentYear = url.searchParams.has('year') ? Number(filterYear) : new Date().getFullYear();
 
 	const reigns = new ReignsRepository();
 	const conn = reigns.conn();
 
-	const availableYears: { year: number }[] = await conn.$queryRaw`SELECT DISTINCT EXTRACT(YEAR FROM won_date) AS year FROM championship_reigns r INNER JOIN championship c ON c.id = r.championship_id WHERE c.type = 'mitb' ORDER BY year DESC`;
+	const years: any = await conn.$queryRaw`SELECT DISTINCT EXTRACT(YEAR FROM won_date) AS year FROM championship_reigns r INNER JOIN championship c ON c.id = r.championship_id WHERE c.type = 'mitb' ORDER BY year DESC`;
+	console.log({ years });
+
+	const availableYears = years.map((y: any) => y.year);
+	const filterReal = availableYears.includes(currentYear) ? currentYear : availableYears[0];
+
 	const currentYearMitbs = await reigns.get({
 		where: {
 			Championship: {
 				type: 'mitb'
 			},
 			won_date: {
-				gte: new Date(currentYear, 0, 1),
-				lte: new Date(currentYear, 11, 31)
+				gte: new Date(filterReal, 0, 1),
+				lte: new Date(filterReal, 11, 31)
 			},
 		},
 		orderBy: {
 			won_date: 'desc'
 		},
-	})
+	});
+
+	console.log({
+		currentYear,
+		currentYearMitbs,
+		availableYears
+	});
+
 
 	return {
 		mitb: {
 			currentYear,
 			reigns: currentYearMitbs,
-			availableYears: availableYears.map(y => y.year)
+			availableYears: availableYears,
 		}
 	};
 }
