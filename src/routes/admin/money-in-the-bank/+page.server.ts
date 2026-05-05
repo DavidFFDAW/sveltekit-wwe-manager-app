c1ba55a2bb15bb3d73e8b1cd5f5a796e34c591bf
DavidFFDAW <davidferflo2@gmail.com>
2026-05-05 13:06:38 +0200

    fix: mitb page error with scalars

--- src/routes/admin/money-in-the-bank/+page.server.ts
+++ src/routes/admin/money-in-the-bank/+page.server.ts
@@ -10,7 +10,7 @@ export const load = async ({ url }) => {
 	const years: any = await conn.$queryRaw`SELECT DISTINCT EXTRACT(YEAR FROM won_date) AS year FROM championship_reigns r INNER JOIN championship c ON c.id = r.championship_id WHERE c.type = 'mitb' ORDER BY year DESC`;
 	console.log({ years });
 
-	const availableYears = years.map((y: any) => y.year);
+	const availableYears = years.map((y: any) => parseInt(y.year, 10));
 	const filterReal = availableYears.includes(currentYear) ? currentYear : availableYears[0];
 
 	const currentYearMitbs = await reigns.get({
