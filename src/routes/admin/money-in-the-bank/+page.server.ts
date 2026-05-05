311f0a53eead8a2edf29ec9ac6bd6557e6c65ba8
DavidFFDAW <davidferflo2@gmail.com>
2026-05-05 10:39:41 +0200

    feat: mitb wip page

---
+++ src/routes/admin/money-in-the-bank/+page.server.ts
@@ -0,0 +1,34 @@
+import { ReignsRepository } from "$lib/server/dao/repositories/reigns.repository";
+
+export const load = async ({ url }) => {
+	const filterYear = url.searchParams.get('year');
+	const currentYear = filterYear ? Number(filterYear) : new Date().getFullYear();
+
+	const reigns = new ReignsRepository();
+	const conn = reigns.conn();
+
+	const availableYears: { year: number }[] = await conn.$queryRaw`SELECT DISTINCT EXTRACT(YEAR FROM won_date) AS year FROM ChampionshipReign WHERE Championship.type = 'mitb' ORDER BY year DESCSELECT DISTINCT EXTRACT(YEAR FROM r.won_date) AS year FROM championship_reigns r INNER JOIN championship c WHERE c.type = 'mitb' ORDER BY year DESC`;
+
+	const currentYearMitbs = await reigns.get({
+		where: {
+			Championship: {
+				type: 'mitb'
+			},
+			won_date: {
+				gte: new Date(currentYear, 0, 1),
+				lte: new Date(currentYear, 11, 31)
+			},
+		},
+		orderBy: {
+			won_date: 'desc'
+		},
+	})
+
+	return {
+		mitb: {
+			currentYear,
+			reigns: currentYearMitbs,
+			availableYears: availableYears.map(y => y.year)
+		}
+	};
+}
---
+++ src/routes/admin/money-in-the-bank/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import Debug from '$lib/components/visual/debug.svelte';
+
+	let { data } = $props();
+	$inspect(data.mitb);
+</script>
+
+<Debug datas={data.mitb} />
