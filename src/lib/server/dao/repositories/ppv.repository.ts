9687e096a87678390f7c49226508a5cfccddfd71
DavidFFDAW <davidferflo2@gmail.com>
2026-05-07 23:14:39 +0200

    fix: brands names uppercase

--- src/lib/server/dao/repositories/ppv.repository.ts
+++ src/lib/server/dao/repositories/ppv.repository.ts
@@ -2,53 +2,53 @@ import { Prisma, type PPV } from '@prisma/client';
 import { Repository } from './Repository';
 
 export class PPVRepository extends Repository<
-	PPV,
-	Prisma.PPVCreateInput,
-	Prisma.PPVUpdateInput,
-	Prisma.PPVWhereInput,
-	Prisma.PPVFindManyArgs,
-	Prisma.PPVGroupByArgs,
-	Prisma.PPVAggregateArgs
+    PPV,
+    Prisma.PPVCreateInput,
+    Prisma.PPVUpdateInput,
+    Prisma.PPVWhereInput,
+    Prisma.PPVFindManyArgs,
+    Prisma.PPVGroupByArgs,
+    Prisma.PPVAggregateArgs
 > {
-	protected requiredFields: string[] = [];
-	private readonly weeklyShows: { name: string; image: string | null }[] = [
-		{
-			name: 'Raw',
-			image: '/brands/raw.webp'
-		},
-		{
-			name: 'SmackDown',
-			image: '/brands/smackdown.webp'
-		},
-		{
-			name: 'AWL',
-			image: '/brands/awl.webp'
-		},
-		{
-			name: 'Thursday Night Main Event',
-			image: '/brands/tnme.webp'
-		}
-	];
+    protected requiredFields: string[] = [];
+    private readonly weeklyShows: { name: string; image: string | null }[] = [
+        {
+            name: 'Raw',
+            image: '/brands/raw.webp'
+        },
+        {
+            name: 'SmackDown',
+            image: '/brands/smackdown.webp'
+        },
+        {
+            name: 'AWL',
+            image: '/brands/awl.webp'
+        },
+        {
+            name: 'Thursday Night Main Event',
+            image: '/brands/tnme.webp'
+        }
+    ];
 
-	private readonly weekly: string[] = ['raw', 'smackdown', 'awl', 'evolution', 'tnme'];
+    private readonly weekly: string[] = ['Raw', 'SmackDown', 'AWL', 'Evolution', 'Thursday Night Main Event'];
 
-	constructor() {
-		super('pPV');
-	}
+    constructor() {
+        super('pPV');
+    }
 
-	getWeeklyShows(): string[] {
-		return this.weeklyShows.map((show) => show.name);
-	}
+    getWeeklyShows(): string[] {
+        return this.weeklyShows.map((show) => show.name);
+    }
 
-	getActiveNames() {
-		return this.get({
-			where: { active: true },
-			select: { name: true },
-			orderBy: { game_date: 'asc' }
-		}).then((results) => [...this.weekly, ...results.map((r) => r.name)]);
-	}
+    getActiveNames() {
+        return this.get({
+            where: { active: true },
+            select: { name: true },
+            orderBy: { game_date: 'asc' }
+        }).then((results) => [...this.weekly, ...results.map((r) => r.name)]);
+    }
 
-	updatePpvDate(id: number, newDate: Date | null): Promise<PPV> {
-		return this.updateById(id, { game_date: newDate });
-	}
+    updatePpvDate(id: number, newDate: Date | null): Promise<PPV> {
+        return this.updateById(id, { game_date: newDate });
+    }
 }
