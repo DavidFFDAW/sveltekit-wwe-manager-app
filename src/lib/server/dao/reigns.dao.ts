2c113afbf34dbd2984afef683c378478957058df
DavidFFDAW <davidferflo2@gmail.com>
2026-06-21 01:14:15 +0200

    feat: last_time_updated is now used only for title defences update_date

--- src/lib/server/dao/reigns.dao.ts
+++ src/lib/server/dao/reigns.dao.ts
@@ -4,195 +4,196 @@ import { getWrestlerOrTeamName, type ChampionshipReignMeta } from '$lib/utils/te
 import { ReignUtils } from '$lib/utils/reign.utils';
 
 export const ReignsDao = {
-	reignExists: async (id: number) => {
-		const reign = await prisma.championshipReign.findUnique({
-			where: {
-				id
-			}
-		});
-		return Boolean(reign?.id);
-	},
-	getChampionshipReigns: () => {
-		return prisma.championshipReign.findMany({
-			orderBy: [
-				{
-					won_date: 'desc'
-				},
-				{
-					Championship: {
-						brand: 'asc'
-					}
-				}
-			],
-			include: {
-				Wrestler: true,
-				Championship: true,
-				Partner: true,
-				Team: true
-			}
-		});
-	},
-	getChampionshipReignByID: (id: number) => {
-		return prisma.championshipReign.findUnique({
-			where: {
-				id
-			},
-			include: {
-				Wrestler: true,
-				Championship: true,
-				Partner: true,
-				Team: true
-			}
-		});
-	},
-	getCurrentReigns: () => {
-		return prisma.championshipReign.findMany({
-			where: {
-				current: true,
-				lost_date: null
-			},
-			include: {
-				Wrestler: true,
-				Championship: true,
-				Partner: true,
-				Team: true
-			},
-			orderBy: [{ won_date: 'desc' }]
-		});
-	},
-	getCurrentChampionshipReign: () => {
-		return prisma.championshipReign.findMany({
-			where: {
-				current: true,
-				lost_date: null,
-				can_stats_count: true,
-				Championship: {
-					active: true
-				}
-			},
-			include: {
-				Wrestler: true,
-				Championship: true,
-				Partner: true,
-				Team: true
-			},
-			orderBy: [{ Championship: { brand: 'asc' } }, { Championship: { order: 'asc' } }]
-		});
-	},
-	getCurrentChampionsDefences: () => {
-		return prisma.championshipReign.findMany({
-			where: {
-				current: true,
-				lost_date: null,
-				can_stats_count: true,
-				Championship: {
-					active: true
-				}
-			},
-			include: {
-				Wrestler: true,
-				Championship: true,
-				Partner: true,
-				Team: true
-			},
-			orderBy: {
-				won_date: 'desc'
-			}
-		});
-	},
-	getChampionshipReignsOrderedWithTeamNames: async (
-		orderBy: Prisma.ChampionshipReignOrderByWithRelationInput = {
-			won_date: 'desc'
-		}
-	) => {
-		const reigns = await prisma.championshipReign.findMany({
-			orderBy: orderBy,
-			include: {
-				Wrestler: true,
-				Championship: true,
-				Partner: true,
-				Team: true
-			}
-		});
-		return reigns.map((reign) => ({
-			...reign,
-			days_month: ReignUtils.getDaysAndMonths(reign.days),
-			team_name: reign.Championship.tag
-				? getWrestlerOrTeamName(reign as ChampionshipReignMeta)
-				: reign.Wrestler.name,
-			won_date_str: reign.won_date.toLocaleDateString('es-ES', {
-				year: 'numeric',
-				month: 'long',
-				day: '2-digit'
-			}),
-			lost_date_str: reign.lost_date
-				? reign.lost_date.toLocaleDateString('es-ES', {
-						year: 'numeric',
-						month: 'long',
-						day: '2-digit'
-					})
-				: ''
-		}));
-	},
+    reignExists: async (id: number) => {
+        const reign = await prisma.championshipReign.findUnique({
+            where: {
+                id
+            }
+        });
+        return Boolean(reign?.id);
+    },
+    getChampionshipReigns: () => {
+        return prisma.championshipReign.findMany({
+            orderBy: [
+                {
+                    won_date: 'desc'
+                },
+                {
+                    Championship: {
+                        brand: 'asc'
+                    }
+                }
+            ],
+            include: {
+                Wrestler: true,
+                Championship: true,
+                Partner: true,
+                Team: true
+            }
+        });
+    },
+    getChampionshipReignByID: (id: number) => {
+        return prisma.championshipReign.findUnique({
+            where: {
+                id
+            },
+            include: {
+                Wrestler: true,
+                Championship: true,
+                Partner: true,
+                Team: true
+            }
+        });
+    },
+    getCurrentReigns: () => {
+        return prisma.championshipReign.findMany({
+            where: {
+                current: true,
+                lost_date: null
+            },
+            include: {
+                Wrestler: true,
+                Championship: true,
+                Partner: true,
+                Team: true
+            },
+            orderBy: [{ won_date: 'desc' }]
+        });
+    },
+    getCurrentChampionshipReign: () => {
+        return prisma.championshipReign.findMany({
+            where: {
+                current: true,
+                lost_date: null,
+                can_stats_count: true,
+                Championship: {
+                    active: true
+                }
+            },
+            include: {
+                Wrestler: true,
+                Championship: true,
+                Partner: true,
+                Team: true
+            },
+            orderBy: [{ Championship: { brand: 'asc' } }, { Championship: { order: 'asc' } }]
+        });
+    },
+    getCurrentChampionsDefences: () => {
+        return prisma.championshipReign.findMany({
+            where: {
+                current: true,
+                lost_date: null,
+                can_stats_count: true,
+                Championship: {
+                    active: true
+                }
+            },
+            include: {
+                Wrestler: true,
+                Championship: true,
+                Partner: true,
+                Team: true
+            },
+            orderBy: {
+                won_date: 'desc'
+            }
+        });
+    },
+    getChampionshipReignsOrderedWithTeamNames: async (
+        orderBy: Prisma.ChampionshipReignOrderByWithRelationInput = {
+            won_date: 'desc'
+        }
+    ) => {
+        const reigns = await prisma.championshipReign.findMany({
+            orderBy: orderBy,
+            include: {
+                Wrestler: true,
+                Championship: true,
+                Partner: true,
+                Team: true
+            }
+        });
+        return reigns.map((reign) => ({
+            ...reign,
+            days_month: ReignUtils.getDaysAndMonths(reign.days),
+            team_name: reign.Championship.tag
+                ? getWrestlerOrTeamName(reign as ChampionshipReignMeta)
+                : reign.Wrestler.name,
+            won_date_str: reign.won_date.toLocaleDateString('es-ES', {
+                year: 'numeric',
+                month: 'long',
+                day: '2-digit'
+            }),
+            lost_date_str: reign.lost_date
+                ? reign.lost_date.toLocaleDateString('es-ES', {
+                    year: 'numeric',
+                    month: 'long',
+                    day: '2-digit'
+                })
+                : ''
+        }));
+    },
 
-	getLastCurrentReignFromChampionship: (championship_id: number) => {
-		return prisma.championshipReign.findFirst({
-			where: {
-				championship_id,
-				current: true,
-				lost_date: null
-			},
-			orderBy: {
-				won_date: 'desc'
-			}
-		});
-	},
+    getLastCurrentReignFromChampionship: (championship_id: number) => {
+        return prisma.championshipReign.findFirst({
+            where: {
+                championship_id,
+                current: true,
+                lost_date: null
+            },
+            orderBy: {
+                won_date: 'desc'
+            }
+        });
+    },
 
-	createChampionshipReign: (
-		data: Prisma.ChampionshipReignUncheckedCreateInput | Prisma.ChampionshipReignCreateInput
-	) => {
-		return prisma.championshipReign.create({
-			data
-		});
-	},
+    createChampionshipReign: (
+        data: Prisma.ChampionshipReignUncheckedCreateInput | Prisma.ChampionshipReignCreateInput
+    ) => {
+        return prisma.championshipReign.create({
+            data
+        });
+    },
 
-	updateChampionshipReign: (
-		id: number,
-		data: Prisma.ChampionshipReignUpdateInput | Prisma.ChampionshipReignUncheckedUpdateInput
-	) => {
-		return prisma.championshipReign.update({
-			where: {
-				id
-			},
-			data
-		});
-	},
+    updateChampionshipReign: (
+        id: number,
+        data: Prisma.ChampionshipReignUpdateInput | Prisma.ChampionshipReignUncheckedUpdateInput
+    ) => {
+        return prisma.championshipReign.update({
+            where: {
+                id
+            },
+            data
+        });
+    },
 
-	updateCurrentChampionshipReignDays: () => {
-		return prisma.championshipReign.updateMany({
-			data: {
-				days: {
-					increment: 1
-				}
-			},
-			where: {
-				AND: [{ lost_date: null }, { current: true }, { won_date: { lte: new Date() } }]
-			}
-		});
-	},
+    updateCurrentChampionshipReignDays: () => {
+        return prisma.championshipReign.updateMany({
+            data: {
+                days: {
+                    increment: 1
+                }
+            },
+            where: {
+                AND: [{ lost_date: null }, { current: true }, { won_date: { lte: new Date() } }]
+            }
+        });
+    },
 
-	updateDefencesFor: (reigns: number[]) => {
-		return prisma.championshipReign.updateMany({
-			where: {
-				id: {
-					in: reigns
-				}
-			},
-			data: {
-				defences: {
-					increment: 1
-				}
-			}
-		});
-	}
+    updateDefencesFor: (reigns: number[]) => {
+        return prisma.championshipReign.updateMany({
+            where: {
+                id: {
+                    in: reigns
+                }
+            },
+            data: {
+                defences: {
+                    increment: 1
+                },
+                last_time_updated: new Date()
+            }
+        });
+    }
 };
--- src/routes/admin/championship-reigns/defences/update/+page.server.ts
+++ src/routes/admin/championship-reigns/defences/update/+page.server.ts
@@ -6,88 +6,88 @@ import { ReignUtils } from '$lib/utils/reign.utils.js';
 const reignsRepo = new ReignsRepository();
 
 export const load = async () => {
-	const reigns = await reignsRepo.get({
-		where: {
-			current: true,
-			lost_date: null,
-			can_stats_count: true,
-			Championship: {
-				active: true
-			}
-		},
-		include: {
-			Wrestler: true,
-			Championship: true,
-			Partner: true,
-			Team: true
-		},
-		orderBy: [
-			{ won_date: 'desc' },
-			{ days: 'desc' }
-		]
-	});
-	
-	return {
-		reigns: reigns.map((reign: any) => {
-			return {
-				...reign,
-				time_days: ReignUtils.getDaysAndMonths(reign.days),
-				ranking_status: ReignUtils.getReignRankingStatus(reign),
-				calculated_name: ReignUtils.getWrestlerOrTeamName(reign),
-				updated_ellapsed: reign.updated_at ? Utils.getEllapsedTime(reign.updated_at) : null
-			};
-		})
-	};
+    const reigns = await reignsRepo.get({
+        where: {
+            current: true,
+            lost_date: null,
+            can_stats_count: true,
+            Championship: {
+                active: true
+            }
+        },
+        include: {
+            Wrestler: true,
+            Championship: true,
+            Partner: true,
+            Team: true
+        },
+        orderBy: [
+            { won_date: 'desc' },
+            { days: 'desc' }
+        ]
+    });
+
+    return {
+        reigns: reigns.map((reign: any) => {
+            return {
+                ...reign,
+                time_days: ReignUtils.getDaysAndMonths(reign.days),
+                ranking_status: ReignUtils.getReignRankingStatus(reign),
+                calculated_name: ReignUtils.getWrestlerOrTeamName(reign),
+                updated_ellapsed: reign.updated_at ? Utils.getEllapsedTime(reign.updated_at) : null
+            };
+        })
+    };
 };
 
 export const actions = {
-	default: async ({ request }) => {
-		const formData = await request.formData();
+    default: async ({ request }) => {
+        const formData = await request.formData();
 
-		try {
-			const reignsRepo = new ReignsRepository();
-			const parsed = Utils.getArrayFormDatas(formData, ['reign-id', 'defenses']);
-			const numericParsedValues = parsed.map((item) => {
-				return {
-					id: parseInt(item['reign-id'], 10),
-					defenses: parseInt(item['defenses'], 10)
-				};
-			});
+        try {
+            const reignsRepo = new ReignsRepository();
+            const parsed = Utils.getArrayFormDatas(formData, ['reign-id', 'defenses']);
+            const numericParsedValues = parsed.map((item) => {
+                return {
+                    id: parseInt(item['reign-id'], 10),
+                    defenses: parseInt(item['defenses'], 10)
+                };
+            });
 
-			const currentReigns = await reignsRepo.toMap({
-				where: {
-					current: true,
-					lost_date: null,
-					can_stats_count: true,
-					Championship: {
-						active: true
-					}
-				}
-			});
+            const currentReigns = await reignsRepo.toMap({
+                where: {
+                    current: true,
+                    lost_date: null,
+                    can_stats_count: true,
+                    Championship: {
+                        active: true
+                    }
+                }
+            });
 
-			const onlyChangedDefenses = numericParsedValues.filter((item) => {
-				const currentReign = currentReigns.get(item.id);
-				if (!currentReign) return false;
-				return currentReign.defences !== item.defenses;
-			});
+            const onlyChangedDefenses = numericParsedValues.filter((item) => {
+                const currentReign = currentReigns.get(item.id);
+                if (!currentReign) return false;
+                return currentReign.defences !== item.defenses;
+            });
 
-			const sameDefensesGroup = onlyChangedDefenses.reduce(
-				(acc, item) => {
-					if (!acc[item.defenses]) acc[item.defenses] = [];
-					acc[item.defenses].push(item.id);
-					return acc;
-				},
-				{} as Record<number, number[]>
-			);
+            const sameDefensesGroup = onlyChangedDefenses.reduce(
+                (acc, item) => {
+                    if (!acc[item.defenses]) acc[item.defenses] = [];
+                    acc[item.defenses].push(item.id);
+                    return acc;
+                },
+                {} as Record<number, number[]>
+            );
 
-			const promises = Object.entries(sameDefensesGroup).map(([defenses, ids]) => {
-				return reignsRepo.bulkUpdate({ id: { in: ids } }, { defences: parseInt(defenses, 10) });
-			});
-			await Promise.all(promises);
-			return Helpers.success('Defensas de los reinados de campeonato actualizadas correctamente');
-		} catch (error) {
-			console.error('Error updating championship reign defence:', error);
-			return Helpers.error('Error actualizando la defensa del reinado del campeonato', 500);
-		}
-	}
+            const promises = Object.entries(sameDefensesGroup).map(([defenses, ids]) => {
+                return reignsRepo.bulkUpdate({ id: { in: ids } }, { defences: parseInt(defenses, 10), last_time_updated: new Date() });
+            });
+            await Promise.all(promises);
+            return Helpers.success('Defensas de los reinados de campeonato actualizadas correctamente');
+        } catch (error) {
+            console.error('Error updating championship reign defence:', error);
+            return Helpers.error('Error actualizando la defensa del reinado del campeonato', 500);
+        }
+    }
 };
