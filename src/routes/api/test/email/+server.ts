c666e9c3ada2a74631036f57eb9e68840fbf632d
DavidFFDAW <davidferflo2@gmail.com>
2026-09-18 15:26:22 +0200

    test: server email test

--- src/routes/api/test/email/+server.ts
+++ src/routes/api/test/email/+server.ts
@@ -1,14 +1,28 @@
 import EmailService from '$lib/server/services/email.server.service';
-import { dirname } from 'path';
+import fs from 'fs/promises';
+
+async function checkIfFile(filePath: string) {
+    try {
+        await fs.access(filePath, fs.constants.F_OK);
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
 
 export const GET = async () => {
     try {
+        const root = process.cwd();
+        const files = await fs.readdir(root);
         const templateRoute = EmailService.getTemplateDirectory('blog');
+        const templateExists = checkIfFile(templateRoute);
+
         return new Response(
             JSON.stringify({
+                root,
+                files,
                 templateRoute,
-                root: process.cwd(),
-                dirname: dirname,
+                templateExists,
             }),
             { status: 200 },
         );
